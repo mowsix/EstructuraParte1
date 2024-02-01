edeede83a59c56e54abbc69392e5d8734a1614b3
Santiago Gallego Henao <109182667+mowsix@users.noreply.github.com>
2024-02-01 10:18:56 -0500

    Create TiposDeColecciones.java

---
+++ src/TiposDeColecciones.java
@@ -0,0 +1,32 @@
+/* 
+Bag()-no importa el orden y solo se agregan
+Queue()- (FIFO) Se agregan y se remueven preservando el orden de llegada
+Stack()- (LIFO) Se remueve en el orden inverso de llegada
+
+
+*/
+public class PilaArreglo {
+
+    private String[] pila;
+    private int n;
+
+    public PilaArreglo(int max) {
+        // TODO: instanciar el arreglo para la pila
+    }
+
+    public void push(String s) {
+        // TODO: Implementar el método
+    }
+
+    public String pop() throws Exception {
+        // TODO: Implementar el método
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    public int size() {
+        return n;
+    }
